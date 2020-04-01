8f54e5b32a1c586c6cb533cc889cc343d635f20b
JoyTailor-1775 <yegor.ivanovskyy@gmail.com>
2020-04-01 23:31:26 +0300

    Updates

---
+++ precache-manifest.9a6506c0e657bd9015b7912bcf7fd47a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c18b297a0bac4cc9bbf579b00977e05e",
+    "url": "/JoyTailor-1775/calendar/index.html"
+  },
+  {
+    "revision": "29c94e10f6159b8a2281",
+    "url": "/JoyTailor-1775/calendar/static/css/2.577802f3.chunk.css"
+  },
+  {
+    "revision": "40eca269ed7bdb0d22b1",
+    "url": "/JoyTailor-1775/calendar/static/css/main.82dc17b4.chunk.css"
+  },
+  {
+    "revision": "29c94e10f6159b8a2281",
+    "url": "/JoyTailor-1775/calendar/static/js/2.bad0125d.chunk.js"
+  },
+  {
+    "revision": "5bfe30a14781fe8297f4672d578d2251",
+    "url": "/JoyTailor-1775/calendar/static/js/2.bad0125d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40eca269ed7bdb0d22b1",
+    "url": "/JoyTailor-1775/calendar/static/js/main.693b54eb.chunk.js"
+  },
+  {
+    "revision": "b87a49a1009fc170011f",
+    "url": "/JoyTailor-1775/calendar/static/js/runtime-main.b34633b2.js"
+  }
+]);
